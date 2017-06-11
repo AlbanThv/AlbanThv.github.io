9b980cc0b0c810021e1762a2b7c18dba3908bfb9
Alban Thouvignon <prof.thouvignon@gmail.com>
2017-06-11 13:21:10 +0200

    Create server.js

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-
+var config = require(__dirname + '/config');
 var express = require('express');
 
 var app = express();
-var server = app.listen(3000);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-app.use(express.static('public'));
+server.listen(config.port);
 
-console.log("My socket server is running");
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+	res.sendfile(__dirname + '/public/index.html');
+});
 
-var socket = require('socket.io');
-
-var io = socket(server);
+console.log(' * Running on http://' + config.server + ':' + config.port.toString());
 
 io.sockets.on('connection', newConnection);
 
-function newConnection(socket) {
-	console.log('new connection: ' + socket.id);
-	
-	socket.on('mouse', mouseMsg)
-	function mouseMsg(data) {
-		socket.broadcast.emit('mouse', data)
-		//io.sockets.emit('mouse', data); broadcast also to self
-		//console.log(data);
-	}
-}
+io.sockets.on('connection', function (socket) {
+    console.log('[*] info: new connection ' + socket.id);
+
+    socket.on('mouse', function (data) {
+        if (data.thickness < 50) {
+            socket.broadcast.emit('mouse', data);
+		    //io.sockets.emit('mouse', data); broadcast also to self
+        }
+    });
+});
