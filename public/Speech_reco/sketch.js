520d9d4b6d052f431d8b171814eecc82342f826e
Alban Thouvignon <prof.thouvignon@gmail.com>
2017-10-23 15:03:41 +0200

    Update sketch.js

--- public/Speech_reco/sketch.js
+++ public/Speech_reco/sketch.js
@@ -1,24 +1,31 @@
+var speechRec = new p5.SpeechRec();
+
 function setup()
 {
-	noCanvas();
+	createCanvas(400, 200);
+	background(0, 255, 0);
+	fill(0, 0, 0, 255);
+	textSize(32);
+	text("say something", width/2, height/2);
 	
 	let voice = new p5.Speech();
 	voice.setVoice('Cellos')
 	voice.speak("Salut tout le monde");
 	
-	let lang = 'en-US'//navigator.language || 'fr-FR';
-	let speechRec = new p5.SpeechRec(lang, gotSpeech);
+	//let lang = 'en-US'//navigator.language || 'fr-FR';
+	speechRec.onResult = gotSpeech;
 	
 	let continuous = true;
 	let interim = true;
 	speechRec.start(continuous, interim);
+	speechRec.onEnd(background(0, 0, 0);)
 }
 
 function gotSpeech()
 {
 	if (speechRec.resultValue)
 	{
-		createP(speechRec.resultString);
+		text(speechRec.resultString, width/2, height/2);
 		console.log(speechRec.resultString);
 	}
 }
