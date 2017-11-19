ed2877e2638fd2cd34973d8924c37eb22590776d
Alban Thouvignon <prof.thouvignon@gmail.com>
2017-11-19 18:50:30 +0100

    Add files via upload

--- public/Super_Mario/js/loaders.js
+++ public/Super_Mario/js/loaders.js
@@ -1,58 +1,23 @@
-import Level from "./Level.js";
-import SpriteSheet from "./SpriteSheet.js";
-import { createBackgroundLayer, createSpriteLayer } from "./layers.js";
-import { createAnim } from "./anim.js";
+import SpriteSheet from './SpriteSheet.js';
+import { createAnim } from './anim.js';
 
 export function loadImage(url) {
   return new Promise(resolve => {
     const image = new Image();
-    image.addEventListener("load", () => {
+    image.addEventListener('load', () => {
       resolve(image);
     });
     image.src = url;
   });
 }
 
-function loadJSON(url) {
+export function loadJSON(url) {
   return fetch(url)
     .then(r => r.json());
 }
 
-function createTiles(level, backgrounds) {
-
-  function applyRanges(background, xStart, xLen, yStart, yLen) {
-    const xEnd = xStart + xLen;
-    const yEnd = yStart + yLen;
-    for (let x = xStart; x < xEnd; ++x) {
-      for (let y = yStart; y < yEnd; ++y) {
-        level.tiles.set(x, y, {
-          name: background.tile,
-          type: background.type,
-        });
-      }
-    }
-  }
-
-  backgrounds.forEach(background => {
-    background.ranges.forEach(range => {
-      if (range.length === 4) {
-        const [xStart, xLen, yStart, yLen] = range;
-        applyRanges(background, xStart, xLen, yStart, yLen);
-
-      } else if (range.length === 3) {
-        const [xStart, xLen, yStart] = range;
-        applyRanges(background, xStart, xLen, yStart, 1);
-
-      } else if (range.length === 2) {
-        const [xStart, yStart] = range;
-        applyRanges(background, xStart, 1, yStart, 1);
-      }
-    });
-  });
-}
-
 export function loadSpriteSheet(name) {
-  return loadJSON(`./sprites/${name}.json`)
+  return loadJSON(`/sprites/${name}.json`)
     .then(sheetSpec => Promise.all([
       sheetSpec,
       loadImage(sheetSpec.imageURL),
@@ -69,44 +34,22 @@ export function loadSpriteSheet(name) {
             tileSpec.name,
             tileSpec.index[0],
             tileSpec.index[1]);
-        })
+        });
       }
 
       if (sheetSpec.frames) {
         sheetSpec.frames.forEach(frameSpec => {
-          sprites.define(frameSpec.name, ...frameSpec.rect)
+          sprites.define(frameSpec.name, ...frameSpec.rect);
         });
       }
 
-      if (sheetSpec.animation) {
-        sheetSpec.animation.forEach(animSpec => {
+      if (sheetSpec.animations) {
+        sheetSpec.animations.forEach(animSpec => {
           const animation = createAnim(animSpec.frames, animSpec.frameLen);
           sprites.defineAnim(animSpec.name, animation);
         });
       }
 
       return sprites;
-    });
-}
-
-
-export function loadLevel(name) {
-  return loadJSON(`./levels/${name}.json`)
-    .then(levelSpec => Promise.all([
-      levelSpec,
-      loadSpriteSheet(levelSpec.spriteSheet),
-    ]))
-    .then(([levelSpec, backgroundSprites]) => {
-      const level = new Level();
-
-      createTiles(level, levelSpec.backgrounds);
-
-      const BackgroundLayer = createBackgroundLayer(level, backgroundSprites);
-      level.comp.layers.push(BackgroundLayer);
-
-      const spriteLayer = createSpriteLayer(level.entities);
-      level.comp.layers.push(spriteLayer);
-
-      return level;
     });
 }
