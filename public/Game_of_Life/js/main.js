af2ddd443d5a48e3d94e5e4bc14ab4b7f861ee70
AxelGuignard <axelguignard@yahoo.fr>
2018-04-25 19:31:29 +0200

    Update js ajout fonction de stockage

--- public/Game_of_Life/js/main.js
+++ public/Game_of_Life/js/main.js
@@ -11,6 +11,9 @@ let btnStep;
 
 let btnClear;
 
+let stored = [];
+let btnStore;
+
 function setup() {
   let cnv = createCanvas(floor((windowWidth-16)/16)*16, floor((windowHeight-16)/16)*16);
   cnv.style('display', 'block');
@@ -19,7 +22,7 @@ function setup() {
   for (let y = tileSize; y < height; y += tileSize) {
     tiles[y/16-1] = [];
     for (let x = tileSize; x < width; x += tileSize) {
-      tiles[y/16-1][x/16-1] = new Tile(x, y);
+      tiles[y/16-1][x/16-1] = new Tile({x, y});
     }
   }
 
@@ -31,6 +34,9 @@ function setup() {
 
   btnClear = document.getElementById('clear');
   btnClear.onclick = fctclear;
+
+  btnStore = document.getElementById('store');
+  btnStore.onclick = store;
 }
 
 function draw() {
@@ -191,16 +197,81 @@ function mouse() {
   }
 }
 
+function store()
+{
+    let newTiles = [[]];
+    for (let i = 0; i < tiles.length; i++) {
+        newTiles[i] = [];
+        for (let j = 0; j < tiles[i].length; j++) {
+            newTiles[i][j] = new Tile(tiles[i][j]);
+        }
+    }
+    stored.push(newTiles);
+
+    btn = document.createElement("button");
+    btn.setAttribute("type", "button");
+    btn.setAttribute("class", stored.length - 1);
+    btn.style.position = "absolute";
+    btn.style.marginLeft = "40px";
+    btn.style.marginTop = stored.length * 30 + 100 + "px";
+    btn.textContent = "Restore " + stored.length;
+    btn.onclick = function()
+    {
+      restore(stored[this.classList["value"]]);
+    };
+
+    del = document.createElement("button");
+    del.setAttribute("type", "button");
+    del.setAttribute("class", stored.length - 1);
+    del.style.position = "absolute";
+    del.style.marginLeft = "10px";
+    del.style.marginTop = stored.length * 30 + 100 + "px";
+    del.textContent = "x";
+    del.onclick = function()
+    {
+      btnDel(this.classList["value"]);
+    };
+
+    document.getElementById("buttons").appendChild(btn);
+    document.getElementById("buttons").appendChild(del);
+}
+
+function restore(storage)
+{
+    let newTiles = [[]];
+    for (let i = 0; i < storage.length; i++) {
+        newTiles[i] = [];
+        for (let j = 0; j < storage[i].length; j++) {
+            newTiles[i][j] = new Tile(storage[i][j]);
+        }
+    }
+    tiles = newTiles;
+}
+
+function btnDel(id)
+{
+  stored[id] = null;
+  document.getElementById("buttons").removeChild(document.getElementsByClassName(id)[0]);
+  document.getElementById("buttons").removeChild(document.getElementsByClassName(id)[0]);
+}
+
+const defaults = {
+    x: 0,
+    y: 0,
+    isAlive: 0,
+    nextStep: 0,
+}
+
 class Tile {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.gridX = (x-tileSize)/tileSize;
-    this.gridY = (y-tileSize)/tileSize;
+  constructor(options) {
+    this.x = options.x || defaults.x;
+    this.y = options.y || defaults.y;
+    this.gridX = (this.x-tileSize)/tileSize;
+    this.gridY = (this.y-tileSize)/tileSize;
 
     this.col = color(0);
-    this.isAlive = 0;
-    this.nextStep = 0;
+    this.isAlive = options.isAlive || defaults.isAlive;
+    this.nextStep = options.nextStep || defaults.nextStep;
   }
 
   show() {
