5e514cf91b857abe613b81235588ba2ef974618b
Alban Thouvignon <prof.thouvignon@gmail.com>
2018-08-07 15:16:31 +0700

    Update main.js

--- public/Demineur/js/main.js
+++ public/Demineur/js/main.js
@@ -186,7 +186,7 @@ function mouseDown(e) {
         document.getElementById('background').style.background = "black";
       }, 200);
       window.navigator.vibrate(100);
-    }, 300);
+    }, 400);
   }
 }
 
