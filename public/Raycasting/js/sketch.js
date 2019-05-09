f27a70e5034f0d319cb1577af964c91a2cb425d1
Alban Thouvignon <prof.thouvignon@gmail.com>
2019-05-09 23:31:42 +0200

    Add files via upload

--- public/Raycasting/js/sketch.js
+++ public/Raycasting/js/sketch.js
@@ -10,6 +10,9 @@ let mouseDown = 0;
 let xoff = 0;
 let yoff = 100000;
 
+let xTrails = [];
+let yTrails = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight - 4);
   for (let i = 0; i < 5; i++) {
@@ -26,6 +29,7 @@ function setup() {
   walls.push(new Boundary(0, height, 0, 0));
 
   particle = new Particle();
+  particle2 = new Particle();
 
   document.onmousedown = function () {
     ++mouseDown;
@@ -49,9 +53,25 @@ function draw() {
     particle.update(noise(xoff) * width, noise(yoff) * height);
     xoff += 0.002;
     yoff += 0.002;
+
+    xTrails.push(particle.pos.x)
+    yTrails.push(particle.pos.y)
+    if (xTrails.length > 100) {
+      xTrails.shift();
+      yTrails.shift();
+    }
   }
 
   particle.show();
   particle.look(walls);
+
+  // particle2.update(mouseX, mouseY);
+  // particle2.show();
+  // particle2.look(walls);
+
+  fill(255,1);
+  for (let i = 0; i < xTrails.length; i++) {
+    ellipse(xTrails[i], yTrails[i], 16)
+  }
 }
 
